7252a24af2bdf965b6780dd25fef0b8a105b6d4e
Sherry Chen <sherryxychen@gmail.com>
2025-09-01 20:41:17 -0700

    work with max_relative_target int / float more robustly

--- src/lerobot/robots/utils.py
+++ src/lerobot/robots/utils.py
@@ -74,7 +74,8 @@ def ensure_safe_goal_position(
 ) -> dict[str, float]:
     """Caps relative action target magnitude for safety."""
 
-    if isinstance(max_relative_target, float):
+    if isinstance(max_relative_target, float) or isinstance(max_relative_target, int):
+        max_relative_target = float(max_relative_target)
         diff_cap = dict.fromkeys(goal_present_pos, max_relative_target)
     elif isinstance(max_relative_target, dict):
         if not set(goal_present_pos) == set(max_relative_target):
